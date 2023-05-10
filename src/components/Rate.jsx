2610901327ba358c134d00a544c9926f2705d654
Saba Komladze <sabakomladze2@gmail.com>
2023-05-10 14:40:07 +0400

    css styles

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -6,7 +6,10 @@ export default function Rate(props) {
 
   return (
     <div className="rate-div">
-      <img src={path} />
+      <div className="img">
+        <img src={path} />
+      </div>
+
       <h1>How did we do?</h1>
       <p>
         Please let us know how we did with your support request. All feedback is
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,7 +2,9 @@ import mobile from "../assets/mobile.png";
 import "./Result.css";
 export default function Result(props){
     return <div className="result">
-        <img src={mobile} />
-        <p className="msg">You selected {props.rate} out of 5</p>
+        <img src={mobile}  className="mobile" />
+        <p>You selected {props.rate} out of 5</p>
+        <h1 className="thank">Thank You!</h1>
+        <p className="msg">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
     </div>
 }
